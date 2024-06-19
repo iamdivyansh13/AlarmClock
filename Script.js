f8c348a34b42c7df3083a2d979068a9f1e6a640f
Divyansh <divyansh132jain@gmail.com>
2024-06-19 15:15:24 +0530

    Initial commit

---
+++ Script.js
@@ -0,0 +1,22 @@
+function updateTime() {
+    const now = new Date();
+    const currentTime = now.toLocaleTimeString();
+    document.getElementById('current-time').innerText = currentTime;
+
+    const alarmTime = document.getElementById('alarm-time').value;
+    if (alarmTime && currentTime === alarmTime + ":00") {
+        document.getElementById('alarm-message').innerText = "Alarm ringing!";
+
+    }
+}
+
+function setAlarm() {
+    const alarmTime = document.getElementById('alarm-time').value;
+    if (alarmTime) {
+        document.getElementById('alarm-message').innerText = "Alarm set for " + alarmTime;
+    } else {
+        document.getElementById('alarm-message').innerText = "Please select a valid time.";
+    }
+}
+
+setInterval(updateTime, 1000);
